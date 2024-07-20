854071c0155df15bb161ea988079eee5184e2db8
bangyejixiang <91167131+Zandaa1@users.noreply.github.com>
2024-07-20 13:45:39 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! AWS Create Contious Delivery Pipeline'));
+app.get('/', (req, res) => res.send('Hello World! AWS Create Contious Delivery Pipeline Module 5'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
